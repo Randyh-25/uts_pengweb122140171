09f36ac1209625346b78bf863f6f71ca30ff0340
Randyh-25 <randy.122140171@student.itera.ac.id>
2025-11-07 17:16:12 +0700

    17: Transient Data Using Sessions

--- quick_tutorial/sessions/tutorial/views.py
+++ quick_tutorial/sessions/tutorial/views.py
@@ -3,22 +3,10 @@ from pyramid.view import (
     view_defaults
     )
 
-
 @view_defaults(renderer='home.pt')
 class TutorialViews:
     def __init__(self, request):
         self.request = request
-
-    @property
-    def counter(self):
-        session = self.request.session
-        if 'counter' in session:
-            session['counter'] += 1
-        else:
-            session['counter'] = 1
-
-        return session['counter']
-
 
     @view_config(route_name='home')
     def home(self):
